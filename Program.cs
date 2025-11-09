26760c5f81bf59324826dc00cb13536688bc30d5
isHardCoded <ishardcoded@gmail.com>
2025-11-09 14:36:10 +0500

    Задание выполнено

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ namespace p511_oop
 {
     internal class Program
     {
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+
         static void Main(string[] args)
         {
+            add(1, 2);
         }
     }
 }
