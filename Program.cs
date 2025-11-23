abc544646410319cbb6a02834891d1b205ca5c45
isHardCoded <ishardcoded@gmail.com>
2025-11-23 13:20:05 +0500

    fix

--- Program.cs
+++ Program.cs
@@ -7,32 +7,12 @@ using System.Threading.Tasks;
 namespace p511_oop
 {
 
-    class User
-    {
-        public string name;
-        public string surname;
-        public int age;
-        public string city;
-
-        public User(string name, string surname, int age, string city)
-        {
-            this.name = name;
-            this.surname = surname;
-            this.age = age;
-            this.city = city;
-        }
-
-        public void Print()
-        {
-            Console.WriteLine($"Name: {name}, Surname: {surname}, Age: {age}, City: {city}");
-        }
-    }
-
+ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Rectangle rectangle = new Rectangle();            
+              
         }
     }
 }
