7dd04e23f2a0ec64afb202b0ab00c696f385c727
isHardCoded <ishardcoded@gmail.com>
2025-12-20 14:50:00 +0500

    Вынесли логику сохранения object в отдельный класс

---
+++ ObjectToTxt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace p511_oop
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+    }
+    internal class ObjectToTxt
+    {
+        public void SaveObject(Person person)
+        {
+            string filePath = @"C:\Users\User\Desktop\data.txt";
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(person.ToString());
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,43 +1,22 @@
-﻿using System;
+﻿using p511_oop;
+using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 
 namespace p511_oop
 {
-    public class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public Person(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name} - {Age}";
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person("John", 25);
-
-
-            string filePath = @"C:\Users\User\Desktop\data.txt";
-
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                writer.WriteLine(person.ToString());
-            }
+            Person person = new Person("Tom", 25);
+            ObjectToTxt save = new ObjectToTxt();
+            
+            save.SaveObject(person);         
         }
     }
 }
