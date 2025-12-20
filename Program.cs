dd62026b6dc8e56d4328e356fcffe3073a17188c
isHardCoded <ishardcoded@gmail.com>
2025-12-20 14:28:58 +0500

    using

--- Program.cs
+++ Program.cs
@@ -9,26 +9,24 @@ using System.IO;
 namespace p511_oop
 {
 
- 
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            StreamWriter streamWriter = null;
             StreamReader streamReader = null;
+
+            string filePath = @"C:\Users\User\Desktop\data.txt";
 
             try
             {
-                streamWriter = new StreamWriter(
-                        @"C:\Users\User\Desktop\data.txt",
-                        append: false,
-                        encoding: Encoding.UTF8
-                );
+                using (StreamWriter streamWriter = new StreamWriter(filePath))
+                {
+                    streamWriter.WriteLine("Hello!");
+                    streamWriter.WriteLine("C#");
 
-                streamWriter.WriteLine("Hello!");
-                streamWriter.WriteLine("C#");
-
-                Console.WriteLine("Данные успешно записаны");
+                    Console.WriteLine("Данные успешно записаны");
+                }                    
             }
             catch (UnauthorizedAccessException ex)
             {
@@ -41,41 +39,25 @@ namespace p511_oop
             catch (IOException ex)
             {
                 Console.WriteLine(ex.Message);
-            } 
+            }
             catch (Exception ex)
             {
                 Console.WriteLine("Неизвестная ошибка");
             }
-
-            finally
-            {
-                if (streamWriter != null)
-                {
-                    streamWriter.Close();
-                }
-            }
         
             try
             {
-                streamReader = new StreamReader(
-                        @"C:\Users\User\Desktop\data.txt",
-                        encoding: Encoding.UTF8
-                );
-
-                string line;
-                while ((line = streamReader.ReadLine()) != null)
+                using (streamReader = new StreamReader(filePath))
                 {
-                    Console.WriteLine(line);
-                }
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }        
             } catch (Exception ex)
             {
                 Console.WriteLine("Неизвестная ошибка");
-            } finally
-            {
-                if (streamReader != null)
-                {
-                    streamReader.Close();
-                }
             }
         }
     }
